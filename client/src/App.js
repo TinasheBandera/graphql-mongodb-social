e87439d2016d1b9d8b8020aa8998b868cd0da9da
BanderaTinashe <beeatee8@gmail.com>
2019-06-30 22:13:26 +0200

    Update changes - 6/30/19

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import 'semantic-ui-css/semantic.min.css'
+import './App.css';
+
+import Menu from './components/Menu';
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+function App() {
+  return (
+    <Router>
+      <Menu/>
+      <Route exact path='/' component={Home} />
+      <Route exact path='/login' component={Login} />
+      <Route exact path='/register' component={Register} />
+    </Router>
+  );
+}
+
+export default App;

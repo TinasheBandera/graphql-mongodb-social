dd40fad3b27f46e99f650cff4c5982e9e1a2f919
BanderaTinashe <beeatee8@gmail.com>
2019-08-20 02:04:34 +0200

    Latest commit 20.1

--- client/src/components/MenuBar.js
+++ client/src/components/MenuBar.js
@@ -1,9 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
 import { Menu } from 'semantic-ui-react'
 import { Link } from 'react-router-dom';
 
+
+import { AuthContext } from '../context/auth';
+
 function MenuBar(){
-  
+  const { user, logout } = useContext(AuthContext);
   const pathname = window.location.pathname;
   const path = pathname === '/' ? 'home' : pathname.substr(1);
 
@@ -11,9 +14,26 @@ function MenuBar(){
 
   const handleItemClick = (e, { name }) =>  setActiveItem(name);
 
-    return (
+  const menuBar = user ? (
+    <Menu pointing secondary size="massive" color="teal">
 
-        <Menu pointing secondary size="massive" color="teal">
+          <Menu.Item
+            name={ user.username }
+            active
+            as={ Link }
+            to='/'
+          />
+         
+          <Menu.Menu position='right'>
+            <Menu.Item
+                name='logout'
+                onClick={logout }
+            />
+            
+          </Menu.Menu>
+        </Menu>
+  ) : (
+    <Menu pointing secondary size="massive" color="teal">
 
           <Menu.Item
             name='home'
@@ -40,9 +60,8 @@ function MenuBar(){
             />
           </Menu.Menu>
         </Menu>
-
-
-    )
+  )
+    return menuBar;
   }
 
 
